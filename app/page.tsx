0b8923a4dafa17533849af6566a55238ee49df28
michaelogclarke <michaelogclarke@gmail.com>
2025-11-11 10:26:59 +0000

    added assets, started carousel and changed some of page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>Welcome to Just Cakes</h1>
-        <p className={styles.description}>
-          Premium custom cakes for all occasions
-        </p>
-      </div>
+          
     </main>
   )
 }
