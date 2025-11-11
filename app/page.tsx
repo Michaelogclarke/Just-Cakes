dc4d7fd76a99b3f25951fc6e6b062f377459bd44
Michaelogclarke <michaelogclarke@gmail.com>
2025-11-11 18:26:05 +0000

    working on products page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { Children } from 'react'
-import Carousel from '../components/Carousel'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <section className={styles.heroSection}>
+     </section>
     </main>
   )
 }
