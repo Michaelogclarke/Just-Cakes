7acac75ee5a76bcfb6c1201f4b66ea957c906882
michaelogclarke <michaelogclarke@gmail.com>
2026-03-19 01:22:02 +0000

    changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['@prisma/client', 'prisma'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
    async headers() {
     return [
       {
